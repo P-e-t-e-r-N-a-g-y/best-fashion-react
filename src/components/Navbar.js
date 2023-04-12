fbe4d13c14accc603c2f080976a4f0ec459063c5
P-e-t-e-r-N-a-g-y <p.nagy9@gmail.com>
2023-04-12 16:26:16 +0200

    Added hamburger icon and all icons imported from react icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React, { useRef } from "react";
 import './Navbar.css';
+import { IoBasketOutline } from "react-icons/io5";
+import { AiOutlineHeart } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { AiOutlineSearch } from "react-icons/ai";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 function Navbar() {
     return (
     <div className="nav-bar-container">
         <div className="nav-bar-left">
-            <div className="nav-bar-element"><h1>Best Fashion</h1></div>
+            <div className="nav-bar-element-left"><h1>Best Fashion</h1></div>
+            <div className="nav-bar-element-left">
+                <img className="favicon-img" src="/favicon.png"></img>
+            </div>
+            <button className="nav-bar-element-left nav-toggle">
+                <RxHamburgerMenu class="hamburger" />
+            </button>
         </div>
         <div className="nav-bar-center">
             <p className="nav-bar-element">WHAT'S NEW</p>
@@ -13,13 +24,24 @@ function Navbar() {
             <p className="nav-bar-element">MEN</p>
         </div>
         <div className="nav-bar-right">
-            <div className="nav-bar-element">
-                <input  className="search-text" type="text" placeholder="Search..." />
-                <button className="search-button" ><img className="icons" src="/search.png" alt="img" /></button>
+            <div className="nav-bar-element-right">
+                <input  className="nav-bar-element-search search-text" type="text" placeholder="Search..." />
+                <button className="nav-bar-element-search search-button" >
+                    <AiOutlineSearch className="icons" />
+                </button>
             </div>
-            <div className="nav-bar-element login-btn"><img className="icons" src="/user_icon.png" /></div>
-            <div className="nav-bar-element"><img className="icons" src="/heart.png" /></div>
-            <div className="nav-bar-element"><img className="icons" src="/basket.png" /><span className="counter">0</span></div>
+            <div className="nav-bar-element-right login-btn">
+                <CgProfile className="icons" />
+            </div>
+            <div className="nav-bar-element-right">
+                <AiOutlineHeart className="icons" />
+            </div>
+            <div className="nav-bar-element-right">
+                <IoBasketOutline className="icons" />
+                <span className="counter">
+                    0
+                </span>
+            </div>
         </div>
     </div>
     )
